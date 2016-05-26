86b25dc3311ee6ca46558904b846d9c307457c0f
Robin Malhotra <malhotra.robin@yahoo.co.in>
2016-05-26 19:58:02 +0530

    added header

--- katex-site/KaTeX/main.js
+++ katex-site/KaTeX/main.js
@@ -1,4 +1,12 @@
+String.prototype.addSlashes = function()
+{
+   return this.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
 window.startup = function() {
+
+    var sigma = document.getElementById('sigma');
+    console.log(sigma.textContent);
+    katex.render("\\displaystyle{" + sigma.textContent + "}", sigma);
     var tex = document.getElementsByClassName("tex");
     Array.prototype.forEach.call(tex, function(el) {
         katex.render(el.getAttribute("data-expr"), el);
@@ -7,6 +15,7 @@ window.startup = function() {
     var demoInput = document.getElementById("demo-input");
     var demoOutput = document.getElementById("demo-output");
 
+    console.log("\\displaystyle{" + demoInput.value + "}");
     function doDemo() {
         katex.render("\\displaystyle{" + demoInput.value + "}", demoOutput);
     }
